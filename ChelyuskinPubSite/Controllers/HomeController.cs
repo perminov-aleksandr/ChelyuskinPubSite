27f57d2bca99ce0fef7a9d3e3aee49dfb5bf2566
perminov-aleksandr <perminov.a@mail.ru>
2016-09-11 16:52:43 +0300

    Add project files.

---
+++ ChelyuskinPubSite/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using ChelyuskinPubSite.Models;
+
+namespace ChelyuskinPubSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            var menu = new MenuViewModel
+            {
+                Sections = new List<MenuSection>
+                {
+                    new MenuSection
+                    {
+                        Name = "Завтраки",
+                        Items = new List<MenuItem>
+                        {
+                            new MenuItem
+                            {
+                                Name = "Омлет с помидорами",
+                                Price = 120,
+                                Currency = Currency.Rub
+                            },
+                            new MenuItem
+                            {
+                                Name = "Овсяная каша",
+                                Price = 95,
+                                Currency = Currency.Rub
+                            },
+                            new MenuItem
+                            {
+                                Name = "Сэндвич с помидрами авокадо и моцареллой",
+                                Price = 220,
+                                Currency = Currency.Rub
+                            }
+                        }
+                    },
+                    new MenuSection
+                    {
+                        Name = "Закуски",
+                        Items = new List<MenuItem>
+                        {
+                            new MenuItem
+                            {
+                                Name = "Копченый сыр сулугуни",
+                                Price = 210,
+                                Currency = Currency.Rub
+                            },
+                            new MenuItem
+                            {
+                                Name = "Мясное ассорти",
+                                Price = 320,
+                                Currency = Currency.Rub
+                            },
+                            new MenuItem
+                            {
+                                Name = "Вяленая оленина",
+                                Price = 350,
+                                Currency = Currency.Rub
+                            }
+                        }
+                    },
+                    new MenuSection
+                    {
+                        Name = "Горячее",
+                        Items = new List<MenuItem>
+                        {
+                            new MenuItem
+                            {
+                                Name = "Свиные ребра BBQ",
+                                Price = 430,
+                                Currency = Currency.Rub
+                            },
+                            new MenuItem
+                            {
+                                Name = "Куриное филе в марокканских специях с кускусом",
+                                Price = 360,
+                                Currency = Currency.Rub
+                            },
+                            new MenuItem
+                            {
+                                Name = "Паста с томатами и сыром",
+                                Price = 340,
+                                Currency = Currency.Rub
+                            }
+                        }
+                    }
+                }
+            };
+            return View(menu);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ChelyuskinPubSite/Models/MenuViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ChelyuskinPubSite.Models
+{
+    [DisplayName("Меню")]
+    public class MenuViewModel
+    {
+        public List<MenuSection> Sections { get; set; }
+    }
+
+    [DisplayName("Раздел меню")]
+    public class MenuSection
+    {
+        public string Name { get; set; }
+        public List<MenuItem> Items { get; set; }
+    }
+
+    [DisplayName("Элемент меню")]
+    public class MenuItem
+    {
+        [DisplayName("Название")]
+        public string Name { get; set; }
+
+        [DisplayName("Объем")]
+        public int Amount { get; set; }
+
+        [DisplayName("Единицы измерения")]
+        public AmountType AmountType { get; set; }
+
+        [DisplayName("Цена")]
+        public int Price { get; set; }
+
+        [DisplayName("Валюта")]
+        public Currency Currency { get; set; }
+
+        [DisplayName("Состав")]
+        public Consist Consist { get; set; }
+
+        [DisplayName("Метки")]
+        public List<string> Tags { get; set; }
+    }
+
+    public enum Currency
+    {
+        Rub, Eur, Usd
+    }
+
+    [DisplayName("Состав")]
+    public class Consist
+    {
+        [DisplayName("Ингридиенты")]
+        public List<string> Elements { get; set; } 
+    }
+
+    [DisplayName("Единица измерения")]
+    public class AmountType
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
